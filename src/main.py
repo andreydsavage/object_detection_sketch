0be3d84e09407b5333d7266be9fe75d9b4207ed3
Andrey <garmrrr@gmail.com>
2026-01-30 15:56:44 +0500

    Delete duplicated models

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from utils.inference import process_video
 from ultralytics import YOLO
 
 VIDEO = '/home/andrey/repositories/object_detection_sketch/data/videos/crowd.mp4'
-MODEL = YOLO('models/yolo26n.pt')
+MODEL = YOLO('src/models/yolo26n.pt')
 TARGET_CLASSES = [0] #person 0 , umbrella 25
 OUTPUT_PATH = '/home/andrey/repositories/object_detection_sketch/data/output/crowd.mp4'
 
